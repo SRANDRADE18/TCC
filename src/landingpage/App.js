b6fca6ae4ba5bd268fd69f2f01171bb459c3d237
SRANDRADE18 <matheus10andrade@gmail.com>
2023-09-06 19:41:27 -0300

    tcc

--- src/landingpage/App.js
+++ src/landingpage/App.js
@@ -15,7 +15,7 @@ import Tenis01 from '../assets/images/tenis_vegano_shoes_esportivo_preto_ 3.png'
 import Estrela from '../assets/images/Vector.svg';
 import PromoTenis01 from '../assets/images/Tenis-info01.png';
 import TenisBranco from '../assets/images/Tenis-Branco.png';
-
+import Pagamrnto from '../assets/images/Pagamento.png'
 
 
 
@@ -231,35 +231,45 @@ export default function landingPage () {
 
       </div>
 
-      <div className='Rodape'>
-        <div className='Traco'></div>
-        
-        <div className='Roda'>
+      <div className='rodape'>
+
+      
+
+        <div className='roda'>
 
           <div className='Social'>
-
             <h1>Social</h1>
             <h2>Facebook</h2>
             <h2>Instagram</h2>
             <h2>Twitter</h2>
             <h2>Whatsapp</h2>
-
           </div>
-
-          <div className='Ajuda'>
-
+          
+          <div className='Social'>
             <h1>Ajuda</h1>
             <h2>Dúvidas Frequentes</h2>
             <h2>Trocas e Devoluções</h2>
             <h2>Atendimentos</h2>
             <h2>Termos de uso</h2>
+          </div>
 
+          <img src={Pagamrnto}></img>
+
+          <div className='Social'>
+            <h1>Entregas</h1>
+            <h2>Frete grátis</h2>
+            <h2>Entrega rápida</h2>
+            <h2>Troca grátis</h2>
           </div>
 
         </div>
-        <div className='Traco'></div>
+
+        
 
       </div>
+      
+
+      
       
 
     </div>
