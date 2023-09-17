b9c0b88cb25a6209eb488f7036ee6175837ada12
geovannaodrigues <130194005+gigigigiz@users.noreply.github.com>
2023-09-17 00:28:41 -0300

    criar conta pronta (ARRMAR LANDING PAGE)

--- src/landingpage/App.js
+++ src/landingpage/App.js
@@ -13,48 +13,49 @@ import Rodape from '../components/Rodape/rodape';
 
 
 
-export default function landingPage () {
+export default function landingPage() {
   return (
-    
+
     <div className="landing-Page">
 
 
-      <Header/>
+      <Header />
 
       <div className='Promo-tenis'>
-      <Link to='/Criar_Conta'> teu pai na minha cama</Link>
+          <img src={PromoTenis}></img>
       </div>
 
+      <Link to='/Criar_Conta'> teu pai na minha cama</Link>
       <div className='Compra-tenis'>
 
         <a href='http://localhost:3000/compra'>
 
-           <div className='Tenis-01'>
+          <div className='Tenis-01'>
 
-         <img className='tenis'  src={Tenis01}></img>
-         <div className='infos-tenis01'>  
+            <img className='tenis' src={Tenis01}></img>
+            <div className='infos-tenis01'>
 
-         <div className='Infos-text'>
-          <h1>Tênis Vegano Shoes  
-            Rocket Sport Preto</h1>
-         </div>
-         
+              <div className='Infos-text'>
+                <h1>Tênis Vegano Shoes
+                  Rocket Sport Preto</h1>
+              </div>
 
-         <div className='estrelas'>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         </div>
 
-         <div className='info-compra'>
-            <h2>Frete grátis</h2>
-            <h3>R$249,90  ou 4x de 60</h3>
-         </div>
+              <div className='estrelas'>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+              </div>
 
-        </div>
-        </div>
+              <div className='info-compra'>
+                <h2>Frete grátis</h2>
+                <h3>R$249,90  ou 4x de 60</h3>
+              </div>
+
+            </div>
+          </div>
 
         </a>
 
@@ -63,61 +64,30 @@ export default function landingPage () {
           <div className='Tenis-01'>
 
 
-         <img className='tenis' src={Tenis01}></img>
-         <div className='infos-tenis01'>  
+            <img className='tenis' src={Tenis01}></img>
+            <div className='infos-tenis01'>
 
-         <div className='Infos-text'>
-          <h1>Tênis Vegano Shoes  
-            Rocket Sport Preto</h1>
-         </div>
-         
+              <div className='Infos-text'>
+                <h1>Tênis Vegano Shoes
+                  Rocket Sport Preto</h1>
+              </div>
 
-         <div className='estrelas'>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         </div>
 
-         <div className='info-compra'>
-            <h2>Frete grátis</h2>
-            <h3>R$249,90  ou 4x de 60</h3>
-         </div>
+              <div className='estrelas'>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+              </div>
 
-         </div>
-         </div>
-          
-        </a>
+              <div className='info-compra'>
+                <h2>Frete grátis</h2>
+                <h3>R$249,90  ou 4x de 60</h3>
+              </div>
 
-        <a href='http://localhost:3000/compra'>
-
-          <div className='Tenis-01'>
-
-         <img className='tenis' src={Tenis01}></img>
-         <div className='infos-tenis01'>  
-
-         <div className='Infos-text'>
-          <h1>Tênis Vegano Shoes  
-            Rocket Sport Preto</h1>
-         </div>
-         
-
-         <div className='estrelas'>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         </div>
-
-         <div className='info-compra'>
-            <h2>Frete grátis</h2>
-            <h3>R$249,90  ou 4x de 60</h3>
-         </div>
-
-        </div>
-        </div>
+            </div>
+          </div>
 
         </a>
 
@@ -125,30 +95,61 @@ export default function landingPage () {
 
           <div className='Tenis-01'>
 
-         <img className='tenis' src={Tenis01}></img>
-         <div className='infos-tenis01'>  
+            <img className='tenis' src={Tenis01}></img>
+            <div className='infos-tenis01'>
 
-         <div className='Infos-text'>
-          <h1>Tênis Vegano Shoes  
-            Rocket Sport Preto</h1>
-         </div>
-         
+              <div className='Infos-text'>
+                <h1>Tênis Vegano Shoes
+                  Rocket Sport Preto</h1>
+              </div>
 
-         <div className='estrelas'>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         <img src={Estrela}></img>
-         </div>
 
-         <div className='info-compra'>
-            <h2>Frete grátis</h2>
-            <h3>R$249,90  ou 4x de 60</h3>
-         </div>
+              <div className='estrelas'>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+              </div>
 
-        </div>
-        </div>
+              <div className='info-compra'>
+                <h2>Frete grátis</h2>
+                <h3>R$249,90  ou 4x de 60</h3>
+              </div>
+
+            </div>
+          </div>
+
+        </a>
+
+        <a href='http://localhost:3000/compra'>
+
+          <div className='Tenis-01'>
+
+            <img className='tenis' src={Tenis01}></img>
+            <div className='infos-tenis01'>
+
+              <div className='Infos-text'>
+                <h1>Tênis Vegano Shoes
+                  Rocket Sport Preto</h1>
+              </div>
+
+
+              <div className='estrelas'>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+                <img src={Estrela}></img>
+              </div>
+
+              <div className='info-compra'>
+                <h2>Frete grátis</h2>
+                <h3>R$249,90  ou 4x de 60</h3>
+              </div>
+
+            </div>
+          </div>
 
 
 
@@ -158,24 +159,24 @@ export default function landingPage () {
 
 
       <div className='Promo-tenis01'>
-      <img src={PromoTenis01}></img>
+        <img src={PromoTenis01}></img>
       </div>
 
       <div className='Tenis-Branco'>
 
         <div className='Textos-103'>
 
-        <div className='Texto1'>
-          <h1>
-          1. Se você se preocupa com os direitos dos animais e deseja evitar o uso de produtos derivados de animais, um tênis vegano é uma escolha adequada.
-          </h1>
-        </div>
+          <div className='Texto1'>
+            <h1>
+              1. Se você se preocupa com os direitos dos animais e deseja evitar o uso de produtos derivados de animais, um tênis vegano é uma escolha adequada.
+            </h1>
+          </div>
 
-        <div className='Texto3'>
-          <h1>
-          3. Evitar substâncias tóxicas: Muitos calçados de couro são tratados com produtos químicos, como corantes e solventes, que podem ser prejudiciais ao meio ambiente e à saúde humana.
-          </h1>
-        </div>
+          <div className='Texto3'>
+            <h1>
+              3. Evitar substâncias tóxicas: Muitos calçados de couro são tratados com produtos químicos, como corantes e solventes, que podem ser prejudiciais ao meio ambiente e à saúde humana.
+            </h1>
+          </div>
 
         </div>
 
@@ -184,13 +185,13 @@ export default function landingPage () {
 
         <div className='Texto1'>
           <h1>
-          2.Sustentabilidade ambiental: Os tênis veganos são geralmente fabricados com materiais sintéticos e alternativos, como poliéster reciclado, borracha natural, lona e microfibras.
+            2.Sustentabilidade ambiental: Os tênis veganos são geralmente fabricados com materiais sintéticos e alternativos, como poliéster reciclado, borracha natural, lona e microfibras.
           </h1>
         </div>
 
       </div>
 
-      <Rodape/>
+      <Rodape />
 
 
     </div>
