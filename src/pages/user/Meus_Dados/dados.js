a60b99f3432bc5cdee6f2b6453b7b5c4b148f5e3
Matheus Dos Santos Andrade <129523267+SRANDRADE18@users.noreply.github.com>
2023-10-22 22:00:52 -0300

    .

--- src/pages/user/Meus_Dados/dados.js
+++ src/pages/user/Meus_Dados/dados.js
@@ -5,8 +5,7 @@ import { useState } from 'react';
 
 import Rodape from '../../../components/Rodape/rodape';
 import Header from '../../../components/Header/header';
-
-
+import { BsPencil } from "react-icons/ai";
 
 
 
@@ -14,7 +13,7 @@ export default function Meusdados() {
 
     return (
 
-        <div className='Pagina-MeusDados0'>
+        <div className='Pagina-MeusDados'>
 
             <Header />
 
@@ -31,53 +30,54 @@ export default function Meusdados() {
 
                 <div className='Conta-Dados'>
 
-                    <img src="../../../../public/assets/images/Minha_Conta/do-utilizador 3.png" />
+                    <img src="/assets/images/Minha_Conta/do-utilizador 3.png" />
                     <h2> OLá, Lidio </h2>
-                    
+
                 </div>
 
                 <div className='Dados-Usuario'>
-                    
+
 
                     <div className='acao-MeusDados'>
 
                         <div className='MeusDados'>
-                            <img src="../../../../public/assets/images/Minha_Conta/monitorando 1.png" />
+                            <img src="/assets/images/Minha_Conta/monitorando 1.png" />
                             <h3>Pedidos</h3>
                         </div>
 
-                        <div className='Linha-Dados'></div>
+
 
                         <div className='MeusDados'>
-                            <img src="../../../../public/assets/images/Minha_Conta/troca 1.png" />
+                            <img src="/assets/images/Minha_Conta/troca 1.png" />
                             <h3>Troca</h3>
                         </div>
 
-                        <div className='Linha-Dados'></div>
+
 
                         <div className='MeusDados'>
-                            <img src="../../../../public/assets/images/Minha_Conta/do-utilizador 3.png" />
+                            <img src="/assets/images/Minha_Conta/do-utilizador 3.png" />
                             <h3>Meus Dados</h3>
                         </div>
 
-                        <div className='Linha-Dados'></div>
+
 
                         <div className='MeusDados'>
-                            <img src="../../../../public/assets/images/Minha_Conta/casa.png" />
+                            <img src="/assets/images/Minha_Conta/casa.png" />
                             <h3>Endereços</h3>
                         </div>
 
-                        <div className='Linha-Dados'></div>
+
 
                         <div className='MeusDados'>
-                            <img src="../../../../public/assets/images/Minha_Conta/cartoes.png" />
+                            <img src="/assets/images/Minha_Conta/cartoes.png" />
                             <h3>cartões Salvos</h3>
                         </div>
 
-                        <div className='Linha-Dados'></div>
+
 
                         <div className='MeusDados'>
-                            <img src="../../../../public/assets/images/Minha_Conta/saida.png" />
+                            <img src="/assets/images/Minha_Conta/saida.png" />
+                            
                             <h3>Sair</h3>
                         </div>
 
@@ -93,30 +93,30 @@ export default function Meusdados() {
 
                                 <div className='Name-dados'>
                                     <p>Nome</p>
-                                    <input className='Input-dados'/>
+                                    <input className='Input-dados'  />
                                 </div>
 
                                 <div className='Name-dados'>
-                                    <p>Data de Nascimento</p>
-                                    <input className='Input-dados' />
+                                    <p>Data Nascimento</p>
+                                    <input className='Input-dados'  />
                                 </div>
 
                                 <div className='Name-dados'>
                                     <p>Senha</p>
-                                    <input className='Input-dados' />
+                                    <input className='Input-dados'  />
                                 </div>
                             </div>
 
-                            <div className='Linhamento-MeusDados'>
+                            <div className='Linhamento-MeusDados02'>
 
                                 <div className='Name-dados'>
                                     <p>CPF</p>
-                                    <input className='Input-dados' />
+                                    <input className='Input-dados'  />
                                 </div>
 
                                 <div className='Name-dados'>
                                     <p>E-mail</p>
-                                    <input className='Input-dados' />
+                                    <input className='Input-dados'  />
                                 </div>
 
                             </div>
