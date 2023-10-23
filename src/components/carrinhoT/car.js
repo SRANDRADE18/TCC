7f9045a21bd051546cb6cd241edf0bd21b9a0d96
SRANDRADE18 <matheus10andrade@gmail.com>
2023-10-23 11:58:51 -0300

    fazer carrinho

--- src/components/carrinhoT/car.js
+++ src/components/carrinhoT/car.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
 import './Cart.scss';
+import CartItem from '../Cartitem/cartitem';
+import AppContext from '../../context/AppContext';
+import formatCurrency from '../../utils/formatCurrency';
 
-import {AiOutlineShoppingCart} from 'react-icons/ai';
+export default function Cart() {
+  const { cartItems, isCartVisible } = useContext(AppContext);
 
-export default function CartButton() {
+  const totalPrice = cartItems.reduce((acc, item) => item.price + acc, 0);
 
-  
-    return (
-      <button type='button' className='cart-button'>
-        <AiOutlineShoppingCart/>
-        <span className='cart-status'>1</span>
-      </button>
-    )
-  
+  return (
+    <section className={`cart ${isCartVisible ? 'cart--active' : ''}`}>
+      <div className="cart-items">
+        { cartItems.map((cartItem) => <CartItem key={cartItem.id} data={cartItem} />) }
+      </div>
 
+      <div className="cart-resume">{formatCurrency(totalPrice, 'BRL')}</div>
+    </section>
+  );
 }
+
 
 
 
