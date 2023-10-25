bb5424ec954f6ec9dd0ae8955af904c70cf651d4
edu <eduardo.sousa2302@gmail.com>
2023-10-25 08:51:12 -0300

    .

--- src/pages/user/Compra_PT2/compra_pt2.js
+++ src/pages/user/Compra_PT2/compra_pt2.js
@@ -7,145 +7,129 @@ import Rodape from '../../../components/Rodape/rodape';
 import Header from '../../../components/Header/header';
 
 
-    export default function compraPt2(){
-        return(
+export default function compraPt2() {
 
-            <div className='pagina-compra-pt2'>
-
-                <Header/>
-
-                <div className='Compra-pt2'>
-                    <h1 className='nome-produto'>Tênis Vegano Shoes Easeful Preto</h1>
-                    <div className='todoMundo'>
-                      <div className='Compra-imgg'>
-
-                        <div className='tenis-Grande'>
-
-                        <img src="../assets/images/tenis/tenis_vegano_shoes_easeful_preto.png"></img>
-
-                       <div className='Tenis-Menores'>
-                         <img className='tenizinho' src="../assets/images/compra_pt2/Tenis-img1.png"/>
-                         <img className='tenizinho' src="../assets/images/compra_pt2/Tenis-img3.png"/>
-                         <img className='tenizinho' src="../assets/images/compra_pt2/Tenis-img4.png"/>
-                       </div>
-                     
-                        </div>
-
-                      </div>
-
-                     <div className='Compra-pt_info'>
-
-                        <h1>Disponibilidade: Imediata</h1>
-                        <h2>DESTAQUE</h2>
-                        <h3>R$ 299,90</h3>
-                        <h4>R$ 284,91 à vista com desconto</h4>
-                        
-                        <div className='Estrela-info-pt2'>
-                          <img src="../assets/images/Vector.svg"></img>
-                          <img src="../assets/images/Vector.svg"></img>
-                          <img src="../assets/images/Vector.svg"></img>
-                          <img src="../assets/images/Vector.svg"></img>
-                          <img src="../assets/images/Vector.svg"></img>
-
-                         </div>
-
-                         <button className='Button-compra'>Comprar</button>
-
-                         <h1>Cores</h1>
-                         <div className='Filtro-Cor-pt2'>
-            
-                          <div className='Preto'></div>
-                         <div className='Roxo' ></div>
-                         <div className='Rosa' ></div>
-
-                         </div>
-
-                         <div className='Filtro-Cor-pt2'>
-
-                          <div className='Marrom'></div>
-                          <div className='Laranja' ></div>
-                          <div className='Verde' ></div>
-
-                         </div>
-
-                         <div className='Comppra-pt_Tamanhos'>
-
-                         <div className='Tamanhos-Definir'>
-                         <h2>35</h2>
-                         </div>
-
-                         <div className='Tamanhos-Definir'>
-                         <h2>36</h2>
-                         </div>
-                         <div className='Tamanhos-Definir'>
-                         
-                         <h2>37</h2>
-                         </div>                       
-                         <div className='Tamanhos-Definir'>
-                         
-                         <h2>38</h2>
-                         </div>                        
-                         <div className='Tamanhos-Definir'>
-                         <h2>39</h2>
-                         </div>
-
-                         </div>
-
-                         <div className='Comppra-pt_Tamanhos'>
-                         <div className='Tamanhos-Definir'>
-                         
-                         <h2>40</h2>
-                         </div>
-                         <div className='Tamanhos-Definir'>
-                         
-                         <h2>41</h2>
-                         </div>
-                         <div className='Tamanhos-Definir'>
-                       
-                         <h2>42</h2>
-                         </div>
-
-                          <div className='Tamanhos-Definir'>
-                       
-                            <h2>43</h2>
-                         </div>
-
-                          <div className='Tamanhos-Definir'>
-                       
-                         <h2>44</h2>
-                         </div>
-                         </div>
+  const tamanhos = [35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
 
 
-                      </div>
-                    </div>
-                </div>
+  return (
 
+    <div className='pagina-compra-pt2'>
 
-                <div className='Descrisão'>
+      <Header />
 
-                    <div className='Descrição-compra'>
+      <div className='Compra-pt2'>
+        <h1 className='nome-produto'>Tênis Vegano Shoes Easeful Preto</h1>
+        <div className='todoMundo'>
+          <div className='Compra-imgg'>
 
-                        <h1>O tênis Easeful da Vegano Shoes é um calçado esportivo vegano que está em alta na moda. Destaca-se pelo seu conforto, leveza e respirabilidade.</h1>
+            <div className='tenis-Grande'>
 
-                    </div>
+              <img src="../assets/images/tenis/tenis_vegano_shoes_easeful_preto.png"></img>
 
-                    <div className='Descrisão-Produto'>
-
-                        <h1>Gênero: Unissex <p></p> Cor:Preto <p></p> Indicado para: Dia a dia<p></p> Pisada: Neutra <p></p> Cabedal: Nylon mesh ray dublado spam 150g<p></p> Forro: Cacharréu espuma de 4mm densidade 28 <p></p>Solado: Micro expandido 3D com sulcos de escoamento de água <p></p>Palmilha: E.V.A 7mm forro em tecido</h1>
-
-                    </div>
-
-                </div>
-
-                <Rodape/>
+              <div className='Tenis-Menores'>
+                <img className='tenizinho' src="../assets/images/compra_pt2/Tenis-img1.png" />
+                <img className='tenizinho' src="../assets/images/compra_pt2/Tenis-img3.png" />
+                <img className='tenizinho' src="../assets/images/compra_pt2/Tenis-img4.png" />
+              </div>
 
             </div>
 
+          </div>
 
-        )
+          <div className='Compra-pt_info'>
+
+            <h1>Disponibilidade: Imediata</h1>
+            <h2>DESTAQUE</h2>
+            <h3>R$ 299,90</h3>
+            <h4>R$ 284,91 à vista com desconto</h4>
+
+            <div className='Estrela-info-pt2'>
+              <img src="../assets/images/Vector.svg"></img>
+              <img src="../assets/images/Vector.svg"></img>
+              <img src="../assets/images/Vector.svg"></img>
+              <img src="../assets/images/Vector.svg"></img>
+              <img src="../assets/images/Vector.svg"></img>
+
+            </div>
+
+            <button className='Button-compra'>Comprar</button>
+
+            <h1>Cores</h1>
+            <div className='Filtro-Cor-pt2'>
+
+              <label className='cores'>
+                <input type='checkbox' />
+                <span className='checkmark'></span>
+              </label>
+
+              <label className='cores'>
+                <input type='checkbox' checked="checked" />
+                <span className='checkmark'></span>
+              </label>
+
+              <label className='cores'>
+                <input type='checkbox' />
+                <span className='checkmark'></span>
+              </label>
+
+              <label className='cores'>
+                <input type='checkbox' />
+                <span className='checkmark'></span>
+              </label>
+
+              <label className='cores'>
+                <input type='checkbox' checked="checked" />
+                <span className='checkmark'></span>
+              </label>
+
+              <label className='cores'>
+                <input type='checkbox' />
+                <span className='checkmark'></span>
+              </label>
+
+            </div>
+
+            <div className='Comppra-pt_Tamanhos'>
+
+              {tamanhos.map(item =>
+                <label className='tamanhos'>
+                  <input type='checkbox' />
+                  <span className='selected'>{item}</span>
+                </label>)}
+
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+      <div className='Descrisão'>
+
+        <div className='Descrição-compra'>
+
+          <h1>O tênis Easeful da Vegano Shoes é um calçado esportivo vegano que está em alta na moda. Destaca-se pelo seu conforto, leveza e respirabilidade.</h1>
+
+        </div>
+
+        <div className='Descrisão-Produto'>
+
+          <h1>Gênero: Unissex <p></p> Cor:Preto <p></p> Indicado para: Dia a dia<p></p> Pisada: Neutra <p></p> Cabedal: Nylon mesh ray dublado spam 150g<p></p> Forro: Cacharréu espuma de 4mm densidade 28 <p></p>Solado: Micro expandido 3D com sulcos de escoamento de água <p></p>Palmilha: E.V.A 7mm forro em tecido</h1>
+
+        </div>
+
+      </div>
+
+      <Rodape />
+
+    </div>
+
+
+  )
 
 
 
-        
-    }
+
+}
