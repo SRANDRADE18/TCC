18b4768703da3771c168694fb9d9ad209539d10f
Matheus Dos Santos Andrade <129523267+SRANDRADE18@users.noreply.github.com>
2023-11-07 23:45:30 -0300

    .

--- src/pages/user/Finalizar/Finalizar.js
+++ src/pages/user/Finalizar/Finalizar.js
@@ -15,18 +15,18 @@ export default function CarrinhoUser() {
 
     const [cep, setCep] = useState('');
     const [resultado, setResultado] = useState(null);
-    
 
-        const consultarCep = async () => {
-            try {
-                const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-                const data = await response.json();
-                setResultado(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-    
+
+    const consultarCep = async () => {
+        try {
+            const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+            const data = await response.json();
+            setResultado(data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
 
 
 
